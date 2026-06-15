1ee89b6d17426a55372a625daa26c864c2a0f8e3
miraitowa6 <2968941520@qq.com>
2026-06-16 07:57:41 +0800

    Java EE 多线程演示代码32

---
+++ system_code/src/thread/Demo32.java
@@ -0,0 +1,16 @@
+package thread;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: CoderYanger
+ * Date: 2026-05-25
+ * Time: 22:24
+ */
+public class Demo32 {
+    public static void main(String[] args) {
+        //TimeUnit.DAYS.sleep(1);
+    }
+}
