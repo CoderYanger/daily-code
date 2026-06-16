8f583ba820fdb9db1f826c46536bb0b5ad739721
miraitowa6 <2968941520@qq.com>
2026-06-16 08:14:11 +0800

    Java EE 多线程演示代码36

---
+++ system_code/src/thread/Demo36.java
@@ -0,0 +1,14 @@
+package thread;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: CoderYanger
+ * Date: 2026-05-27
+ * Time: 10:36
+ */
+public class Demo36 {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
