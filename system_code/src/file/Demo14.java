eccbc85ff04919c5eb7d25a7c92ac791e5b8f20d
miraitowa6 <2968941520@qq.com>
2026-06-30 00:09:43 +0800

    对文件名及文件内容进行搜索

---
+++ system_code/src/file/Demo14.java
@@ -0,0 +1,79 @@
+package file;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: CoderYanger
+ * Date: 2026-06-08
+ * Time: 23:00
+ */
+//对文件名及文件内容进行搜索
+public class Demo14 {
+    public static void main(String[] args) {
+        //还是先做检查校验
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("请输入要搜索的路径：");
+        String rootPath=scanner.next();
+        File rootFile=new File(rootPath);
+        if(!rootFile.isDirectory()){
+            System.out.println("输入的路径不是目录");
+            return;
+        }
+        System.out.println("请输入要搜索的关键字：");
+        String keyword=scanner.next();
+
+        scanDir(rootFile,keyword);
+    }
+
+    private static void scanDir(File rootFile, String keyword) {
+        //1.列出当前目录下所有的内容
+        File[] files=rootFile.listFiles();
+        if(files==null){
+            //当前目录为空，直接返回
+            return;
+        }
+        for(File file:files){
+            if(file.isFile()){
+                //是普通文件
+                dealFile(file,keyword);
+            }else{
+                //是目录,递归调用
+                scanDir(file,keyword);
+            }
+        }
+    }
+    private static void dealFile(File file, String keyword) {
+        //1.判断当前文件是否包含关键字
+        if(file.getName().contains(keyword)){
+            System.out.println("文件名包含关键字："+file.getAbsolutePath());
+            return;
+        }
+        //2.判断文件内容是否包含关键字,由于 keyword 是字符串，就按照字符流的方式来处理
+        StringBuilder stringBuilder=new StringBuilder();
+        try(Reader reader=new FileReader(file)){
+            while(true){
+                char[] chars=new char[1024];
+                int n=reader.read(chars);
+                if(n==-1){
+                    //文件读完
+                    break;
+                }
+                //将字符数组全追加到stringBuilder中
+                stringBuilder.append(chars,0,n);
+            }
+        }catch(IOException e){
+            throw new RuntimeException(e);
+        }
+        //3.判断 stringBuilder 中是否包含关键字
+        if(stringBuilder.indexOf(keyword)>=0){
+            System.out.println("文件内容包含关键字："+file.getAbsolutePath());
+        }
+        return;
+    }
+}
